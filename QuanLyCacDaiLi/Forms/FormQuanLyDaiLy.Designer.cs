e6913bdb581eea74e5b86a88a7faccc3108026d6
Phan Quý Nguyên <pqn2810@gmail.com>
2021-06-28 20:17:59 +0700

    Update FormQuanLyDaiLy.Designer.cs

--- QuanLyCacDaiLi/Forms/FormQuanLyDaiLy.Designer.cs
+++ QuanLyCacDaiLi/Forms/FormQuanLyDaiLy.Designer.cs
@@ -30,19 +30,24 @@ namespace QuanLyCacDaiLi
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.groupBoxDanhSachDaiLy = new System.Windows.Forms.GroupBox();
+            this.dataGridViewDanhSachDaiLy = new System.Windows.Forms.DataGridView();
+            this.groupBoxTimDaiLy = new System.Windows.Forms.GroupBox();
             this.textBoxTimDaiLyData = new System.Windows.Forms.TextBox();
             this.textBoxTimDaiLy = new System.Windows.Forms.TextBox();
-            this.textBoxTitle = new System.Windows.Forms.TextBox();
-            this.dataGridViewDanhSachDaiLy = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.groupBoxDanhSachDaiLy.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDanhSachDaiLy)).BeginInit();
+            this.groupBoxTimDaiLy.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
+            this.splitContainer1.BackColor = System.Drawing.Color.Gray;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
@@ -50,41 +55,19 @@ namespace QuanLyCacDaiLi
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.textBoxTimDaiLyData);
-            this.splitContainer1.Panel1.Controls.Add(this.textBoxTimDaiLy);
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
             this.splitContainer1.Panel1.Controls.Add(this.textBoxTitle);
             this.splitContainer1.Panel1MinSize = 200;
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.dataGridViewDanhSachDaiLy);
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.splitContainer1.Panel2.Controls.Add(this.groupBoxDanhSachDaiLy);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBoxTimDaiLy);
             this.splitContainer1.Size = new System.Drawing.Size(782, 476);
             this.splitContainer1.SplitterDistance = 236;
+            this.splitContainer1.SplitterWidth = 20;
             this.splitContainer1.TabIndex = 11;
-            // 
-            // textBoxTimDaiLyData
-            // 
-            this.textBoxTimDaiLyData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.textBoxTimDaiLyData.Font = new System.Drawing.Font("Nunito", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBoxTimDaiLyData.Location = new System.Drawing.Point(146, 194);
-            this.textBoxTimDaiLyData.Name = "textBoxTimDaiLyData";
-            this.textBoxTimDaiLyData.Size = new System.Drawing.Size(422, 39);
-            this.textBoxTimDaiLyData.TabIndex = 12;
-            this.textBoxTimDaiLyData.TextChanged += new System.EventHandler(this.textBoxTimDaiLy_TextChanged);
-            // 
-            // textBoxTimDaiLy
-            // 
-            this.textBoxTimDaiLy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.textBoxTimDaiLy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
-            this.textBoxTimDaiLy.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxTimDaiLy.Enabled = false;
-            this.textBoxTimDaiLy.Font = new System.Drawing.Font("Nunito SemiBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBoxTimDaiLy.Location = new System.Drawing.Point(5, 193);
-            this.textBoxTimDaiLy.Name = "textBoxTimDaiLy";
-            this.textBoxTimDaiLy.Size = new System.Drawing.Size(135, 37);
-            this.textBoxTimDaiLy.TabIndex = 11;
-            this.textBoxTimDaiLy.Text = "Tìm đại lý";
-            this.textBoxTimDaiLy.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxTitle
             // 
@@ -102,18 +85,67 @@ namespace QuanLyCacDaiLi
             this.textBoxTitle.Text = "Danh Sách Các Đại Lý";
             this.textBoxTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // groupBoxDanhSachDaiLy
+            // 
+            this.groupBoxDanhSachDaiLy.Controls.Add(this.dataGridViewDanhSachDaiLy);
+            this.groupBoxDanhSachDaiLy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxDanhSachDaiLy.Location = new System.Drawing.Point(0, 60);
+            this.groupBoxDanhSachDaiLy.Name = "groupBoxDanhSachDaiLy";
+            this.groupBoxDanhSachDaiLy.Size = new System.Drawing.Size(782, 160);
+            this.groupBoxDanhSachDaiLy.TabIndex = 3;
+            this.groupBoxDanhSachDaiLy.TabStop = false;
+            this.groupBoxDanhSachDaiLy.Text = "Danh sách đại lý";
+            // 
             // dataGridViewDanhSachDaiLy
             // 
+            this.dataGridViewDanhSachDaiLy.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewDanhSachDaiLy.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
+            this.dataGridViewDanhSachDaiLy.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewDanhSachDaiLy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewDanhSachDaiLy.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridViewDanhSachDaiLy.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewDanhSachDaiLy.Location = new System.Drawing.Point(3, 23);
             this.dataGridViewDanhSachDaiLy.Name = "dataGridViewDanhSachDaiLy";
             this.dataGridViewDanhSachDaiLy.RowHeadersWidth = 51;
             this.dataGridViewDanhSachDaiLy.RowTemplate.Height = 29;
-            this.dataGridViewDanhSachDaiLy.Size = new System.Drawing.Size(782, 236);
+            this.dataGridViewDanhSachDaiLy.Size = new System.Drawing.Size(776, 134);
             this.dataGridViewDanhSachDaiLy.TabIndex = 1;
             // 
-            // TraCuuDaiLy
+            // groupBoxTimDaiLy
+            // 
+            this.groupBoxTimDaiLy.Controls.Add(this.textBoxTimDaiLyData);
+            this.groupBoxTimDaiLy.Controls.Add(this.textBoxTimDaiLy);
+            this.groupBoxTimDaiLy.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBoxTimDaiLy.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxTimDaiLy.Name = "groupBoxTimDaiLy";
+            this.groupBoxTimDaiLy.Size = new System.Drawing.Size(782, 60);
+            this.groupBoxTimDaiLy.TabIndex = 2;
+            this.groupBoxTimDaiLy.TabStop = false;
+            // 
+            // textBoxTimDaiLyData
+            // 
+            this.textBoxTimDaiLyData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxTimDaiLyData.Font = new System.Drawing.Font("Nunito", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxTimDaiLyData.Location = new System.Drawing.Point(153, 15);
+            this.textBoxTimDaiLyData.Name = "textBoxTimDaiLyData";
+            this.textBoxTimDaiLyData.Size = new System.Drawing.Size(422, 39);
+            this.textBoxTimDaiLyData.TabIndex = 12;
+            this.textBoxTimDaiLyData.TextChanged += new System.EventHandler(this.textBoxTimDaiLy_TextChanged);
+            // 
+            // textBoxTimDaiLy
+            // 
+            this.textBoxTimDaiLy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxTimDaiLy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.textBoxTimDaiLy.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxTimDaiLy.Enabled = false;
+            this.textBoxTimDaiLy.Font = new System.Drawing.Font("Nunito SemiBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxTimDaiLy.Location = new System.Drawing.Point(12, 17);
+            this.textBoxTimDaiLy.Name = "textBoxTimDaiLy";
+            this.textBoxTimDaiLy.Size = new System.Drawing.Size(135, 37);
+            this.textBoxTimDaiLy.TabIndex = 11;
+            this.textBoxTimDaiLy.Text = "Tìm đại lý";
+            this.textBoxTimDaiLy.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // FormQuanLyDaiLy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -121,7 +153,7 @@ namespace QuanLyCacDaiLi
             this.ClientSize = new System.Drawing.Size(782, 476);
             this.Controls.Add(this.splitContainer1);
             this.MinimumSize = new System.Drawing.Size(800, 500);
-            this.Name = "TraCuuDaiLy";
+            this.Name = "FormQuanLyDaiLy";
             this.Text = "TraCuuDaiLy";
             this.Load += new System.EventHandler(this.TraCuuDaiLy_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -129,7 +161,10 @@ namespace QuanLyCacDaiLi
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.groupBoxDanhSachDaiLy.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDanhSachDaiLy)).EndInit();
+            this.groupBoxTimDaiLy.ResumeLayout(false);
+            this.groupBoxTimDaiLy.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -140,5 +175,7 @@ namespace QuanLyCacDaiLi
         private System.Windows.Forms.DataGridView dataGridViewDanhSachDaiLy;
         private System.Windows.Forms.TextBox textBoxTimDaiLyData;
         private System.Windows.Forms.TextBox textBoxTimDaiLy;
+        private System.Windows.Forms.GroupBox groupBoxTimDaiLy;
+        private System.Windows.Forms.GroupBox groupBoxDanhSachDaiLy;
     }
 }
